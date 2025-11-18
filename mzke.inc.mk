f79d9d2313d90d2ce3c071f99a69b650b41d2c6d
Joseph Cheek <joseph@ziquid.com>
2025-11-18 16:14:44 -0700

    add support for dirnames with spaces in project dir, copy example Mzkefile.

--- mzke.inc.mk
+++ mzke.inc.mk
@@ -4,7 +4,8 @@ FEATURES := local-install
 LOCAL_INSTALL_ENTRIES := \
     src/bin/mzke:/usr/local/bin:0755 \
     src/mk/features/*.inc.mk:/usr/local/share/mzke/mk/features:0644 \
-    src/mk/*.inc.mk:/usr/local/share/mzke/mk:0644
+    src/mk/*.inc.mk:/usr/local/share/mzke/mk:0644 \
+    src/Mzkefile.example:/usr/local/share/mzke:0644
 
 # Default target
 ALL_TARGET := help
