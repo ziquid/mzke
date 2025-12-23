0b98fdb34ae47efe6ad44d9b0723f02fce7655d9
Joseph Cheek <joseph@ziquid.com>
2025-12-22 20:27:58 -0700

    Add npm feature for mzke
    Provides convenient targets for common npm operations:
    - install/i: Install dependencies
    - pack/p: Create tarball
    - publish/pub: Publish to registry
    - run/start: Run npm start
    - run-dev/start-dev/rd/sd: Run npm dev script
    - test/t: Run tests
    
    All targets support argument passing via ARGS variable.
    
    Co-Authored-By: Tim <tim@ai.zds.group>

---
+++ src/mk/features/npm.inc.mk
@@ -0,0 +1,30 @@
+# features/npm.inc.mk
+
+# NPM package management and script execution
+###
+FEATURE_NPM := Y
+
+# Main targets
+.PHONY: install i
+install i: ## Install npm dependencies
+	npm install $($@_ARGS)
+
+.PHONY: pack p
+pack p: ## Create a tarball from the package
+	npm pack $($@_ARGS)
+
+.PHONY: publish pub
+publish pub: ## Publish package to npm registry
+	npm publish $($@_ARGS)
+
+.PHONY: run start
+run start: ## Run 'npm start' with optional arguments
+	npm start $($@_ARGS)
+
+.PHONY: run-dev start-dev rd sd
+run-dev start-dev rd sd: ## Run 'npm run dev' for development mode
+	npm run dev $($@_ARGS)
+
+.PHONY: test t
+test t: ## Run npm tests
+	npm test $($@_ARGS)
