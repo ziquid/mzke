7763ff9e3ac0d2f2388f59322ecba09147a3e451
Joseph Cheek <joseph@ziquid.com>
2026-01-16 20:20:55 -0700

    image feature uses target build-image, not build, now.

--- src/mk/features/image.inc.mk
+++ src/mk/features/image.inc.mk
@@ -7,7 +7,7 @@ FEATURE_IMAGE := Y
 DOCKERFILE ?= Dockerfile
 
 # Set progress format to rawjson for AI agent contexts
-ifneq ($(or $(CLAUDE_CODE_ENTRYPOINT),$(ZDS_AI_AGENT_BOT_NAME)),)
+ifneq ($(or $(CLAUDE_CODE_ENTRYPOINT),$(ZDS_AI_AGENT_HANDLE)),)
   PROGRESS := --progress plain
 endif
 
@@ -16,8 +16,8 @@ ifneq ($(BUILD_ARGS),)
   BUILD_ARGS_FLAGS = $(foreach arg,$(BUILD_ARGS),--build-arg $(arg) )
 endif
 
-.PHONY: bpa build
-bpa build: $(IMAGE_BUILD_DEPS) $(DOCKERFILE) ddr ## Build and Push All custom Docker image(s) built by this app
+.PHONY: build-image
+build-image: $(IMAGE_BUILD_DEPS) $(DOCKERFILE) ddr ## Build and Push All custom Docker image(s) built by this app
 ifeq ($(IS_PROD),Y)
 	$(eval IMAGE_BUILD_PUSH := --push)
 	$(eval IMAGE_BUILD_PLATFORMS := --platform linux/amd64,linux/arm64)
