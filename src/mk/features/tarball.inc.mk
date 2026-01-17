881ae9a00229b950a366d3eb33277584a521caf7
Joseph Cheek <joseph@ziquid.com>
2026-01-16 22:28:43 -0700

    Updated tarball feature to pack submodules as well.

--- src/mk/features/tarball.inc.mk
+++ src/mk/features/tarball.inc.mk
@@ -8,11 +8,19 @@ TAR_EXCLUDE_MAC_METADATA := $(if $(IS_MAC),--no-mac-metadata,)
 
 .PHONY: package pack p tarball tar tb
 package pack p tarball tar tb: build ## Create the $(APP)-source.tgz package.  Implies build.
-	tar cz --no-xattrs $(TAR_EXCLUDE_MAC_METADATA) --exclude-vcs -C .. --exclude $(APP)-source.tgz --exclude $(APP).tgz -f $(APP)-source.tgz $(APP)
+	$(eval TMPFILE := $(shell mktemp))
+	git ls-files --recurse-submodules | \
+	  while read a; do \
+	    printf $(APP)/%s'\n' $$a; \
+	  done > $(TMPFILE)
+	tar cvz --no-xattrs $(TAR_EXCLUDE_MAC_METADATA) --exclude-vcs -C .. --exclude $(APP)/$(APP)-source.tgz --exclude $(APP)/$(APP).tgz -f $(APP)-source.tgz -T $(TMPFILE)
+	rm -f $(TMPFILE)
+# 	git archive --format=tar.gz -o $(APP)-source.tgz --prefix $(APP)/ HEAD
 
 ifdef FEATURE_LOCAL_INSTALL
 .PHONY: pack-install-global pack-install pig pi
-pack-install-global pack-install pig pi: install
+pack-install-global pack-install pig pi: pack install ## pack and install
+	@:
 
 .PHONY: prig
 prig: pig ## alias for pig
