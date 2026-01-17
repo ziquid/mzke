6d504b554c1ede1a6567c2d1eb3bdbe1ed3d5b52
Joseph Cheek <joseph@ziquid.com>
2026-01-16 22:30:18 -0700

    make the `pack` target in tarball feature a little less verbose.

--- src/mk/features/tarball.inc.mk
+++ src/mk/features/tarball.inc.mk
@@ -9,12 +9,12 @@ TAR_EXCLUDE_MAC_METADATA := $(if $(IS_MAC),--no-mac-metadata,)
 .PHONY: package pack p tarball tar tb
 package pack p tarball tar tb: build ## Create the $(APP)-source.tgz package.  Implies build.
 	$(eval TMPFILE := $(shell mktemp))
-	git ls-files --recurse-submodules | \
+	@git ls-files --recurse-submodules | \
 	  while read a; do \
 	    printf $(APP)/%s'\n' $$a; \
 	  done > $(TMPFILE)
-	tar cvz --no-xattrs $(TAR_EXCLUDE_MAC_METADATA) --exclude-vcs -C .. --exclude $(APP)/$(APP)-source.tgz --exclude $(APP)/$(APP).tgz -f $(APP)-source.tgz -T $(TMPFILE)
-	rm -f $(TMPFILE)
+	tar cz --no-xattrs $(TAR_EXCLUDE_MAC_METADATA) --exclude-vcs -C .. --exclude $(APP)/$(APP)-source.tgz --exclude $(APP)/$(APP).tgz -f $(APP)-source.tgz -T $(TMPFILE)
+	@rm -f $(TMPFILE)
 # 	git archive --format=tar.gz -o $(APP)-source.tgz --prefix $(APP)/ HEAD
 
 ifdef FEATURE_LOCAL_INSTALL
